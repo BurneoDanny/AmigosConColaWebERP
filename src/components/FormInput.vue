0e043b0e430313496328d9c6362d35558a4783cb
Cristopher Villa <cgvilla@espol.edu.ec>
2024-07-26 00:50:35 -0500

    Add: Input and Select component (#34)
    * Add: Input and Select component
    
    * Fix: Components input and select

---
+++ src/components/FormInput.vue
@@ -0,0 +1,7 @@
+<template>
+  <input
+    class="`block w-full text-base font-medium bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary focus:border-primary p-2.5`"
+    autocomplete="off"
+    v-bind="$attrs"
+  />
+</template>
