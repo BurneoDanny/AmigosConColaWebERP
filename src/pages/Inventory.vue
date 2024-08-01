36c46d952e4e8a48e834e492d7271e0bb13166a1
Burneo Danny <dannyburneo2002@hotmail.com>
2024-07-31 23:36:00 -0500

    REady for Kevin

--- src/pages/Inventory.vue
+++ src/pages/Inventory.vue
@@ -12,35 +12,36 @@ import { useRouter } from "vue-router";
 </script>
 
 <template>
-  <div class="flex flex-col justify-center">
-    <div
-      class="flex justify-start lg:justify-end items-center gap-3 lg:mx-4 mt-5"
-    >
-      <SearchInput v-model="search" />
-      <AddButton @click="onRegisterAnimalClicked">
-        <span class="hidden md:flex">Agregar al inventario</span>
-      </AddButton>
-    </div>
-    <div class="mt-5 mb-5 flex items-center gap-3">
-      <h2 class="text-2xl md:text-4xl font-bold m-0">Inventario</h2>
-    </div>
-    <div class="flex items-center flex-wrap gap-3 mt-4 mb-7">
-      <FilterButton
-        :icon="filterIcon"
-        :isSelect="isDogSelect"
-        text="Filtrar"
-        @click="selectDogs"
-      />
-      <FilterButton
-        :icon="pillIcon"
-        :isSelect="isCatSelect"
-        text="Medicinas"
-        @click="selectCats"
-      />
-    </div>
-
-    <section class="gap-x-5 flex md:gap-x-5 lg:gap-x-11 gap-y-9 flex-wrap mt-2">
-        <InventoryTable/>
+  <div class="flex flex-col justify-center h-screen">
+    <section class="flex-none">
+      <div
+        class="flex justify-start lg:justify-end items-center gap-3 lg:mx-4 mt-5"
+      >
+        <SearchInput v-model="search" />
+        <AddButton @click="onRegisterAnimalClicked">
+          <span class="hidden md:flex">Agregar al inventario</span>
+        </AddButton>
+      </div>
+      <div class="mt-5 mb-5 flex items-center gap-3">
+        <h2 class="text-2xl md:text-4xl font-bold m-0">Inventario</h2>
+      </div>
+      <div class="flex items-center flex-wrap gap-3 mt-4 mb-7">
+        <FilterButton
+          :icon="filterIcon"
+          :isSelect="isDogSelect"
+          text="Filtrar"
+          @click="selectDogs"
+        />
+        <FilterButton
+          :icon="pillIcon"
+          :isSelect="isCatSelect"
+          text="Medicinas"
+          @click="selectCats"
+        />
+      </div>
+    </section>
+    <section class="flex-1 gap-x-5 flex md:gap-x-5 lg:gap-x-11 gap-y-9 flex-wrap mt-2 mb-2">
+      <InventoryTable />
     </section>
   </div>
 </template>
