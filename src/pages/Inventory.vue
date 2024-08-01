c872525c25cf8f0e41213978085eab941c43151e
Burneo Danny <dannyburneo2002@hotmail.com>
2024-07-31 20:57:42 -0500

    table in progress

--- src/pages/Inventory.vue
+++ src/pages/Inventory.vue
@@ -2,9 +2,9 @@
 import AddButton from "@/components/home_page/AddButton.vue";
 import SearchInput from "@/components/home_page/SearchInput.vue";
 import FilterButton from "@/components/home_page/FilterButton.vue";
-import AnimalCard from "@/components/home_page/AnimalCard.vue";
+import InventoryTable from "@/components/home_page/InventoryTable.vue";
 import filterIcon from "@/assets/home_page/filter.svg";
-import dogIcon from "@/assets/home_page/dog.svg";
+import pillIcon from "@assets/home_page/pill.svg";
 import { ref, watch, computed, ComputedRef } from "vue";
 import { Animal, useAnimals } from "@stores/animalStore.ts";
 import { AnimalSpecies } from "@/enums/animal_species.ts";
@@ -23,11 +23,6 @@ import { useRouter } from "vue-router";
     </div>
     <div class="mt-5 mb-5 flex items-center gap-3">
       <h2 class="text-2xl md:text-4xl font-bold m-0">Inventario</h2>
-      <img
-        :src="bellIcon"
-        alt="notification bell"
-        class="mt-1 md:mt-2 size-4 md:size-6"
-      />
     </div>
     <div class="flex items-center flex-wrap gap-3 mt-4 mb-7">
       <FilterButton
@@ -37,7 +32,7 @@ import { useRouter } from "vue-router";
         @click="selectDogs"
       />
       <FilterButton
-        :icon="filterIcon"
+        :icon="pillIcon"
         :isSelect="isCatSelect"
         text="Medicinas"
         @click="selectCats"
@@ -45,14 +40,7 @@ import { useRouter } from "vue-router";
     </div>
 
     <section class="gap-x-5 flex md:gap-x-5 lg:gap-x-11 gap-y-9 flex-wrap mt-2">
-      <RouterLink
-        v-for="animal in animalesFiltered"
-        :key="animal.id"
-        :to="`/pet-info/${animal.id}`"
-        class="gap-x-5 flex md:gap-x-5 lg:gap-x-11 gap-y-9 flex-wrap mt-2"
-      >
-        <AnimalCard :animal="animal" />
-      </RouterLink>
+        <InventoryTable/>
     </section>
   </div>
 </template>
