c9b8ae8492982ebb259e9a3cace441a34d3dc28f
Alina Carpio <72272077+alicarpio@users.noreply.github.com>
2024-08-02 22:33:22 -0500

    Change animal card component (#38)
    * Add SideBar, NavBar components
    
    * Change animal card component
    
    ---------
    
    Co-authored-by: Alexander Goussas <84427521+aloussase@users.noreply.github.com>

--- src/components/home_page/AnimalCard.vue
+++ src/components/home_page/AnimalCard.vue
@@ -1,52 +1,53 @@
 <script lang="ts" setup>
-import maleIcon from "../../assets/home_page/male.svg";
-import femaleIcon from "../../assets/home_page/female.svg";
 import { Animal } from "@stores/animalStore.ts";
-import { AnimalGender } from "@/enums/animal_gender.ts";
 import dogUrl from "@/assets/images/dog_default.jpg";
 import catUrl from "@/assets/images/cat_default.jpg";
 import { AnimalSpecies } from "@/enums/animal_species.ts";
+import AnimalGenderIcon from "@/components/home_page/AnimalGenderIcon.vue";
 
 const { animal } = defineProps<{ animal: Animal }>();
 
-const genero = animal.genero === AnimalGender.MALE ? maleIcon : femaleIcon;
 const adoptado = animal.adoptado ? "ADOPTADO" : "NO ADOPTADO";
 </script>
 
 <template>
-  <a
-    class="size-40 md:w-[15rem] h-max max-w-sm bg-white border border-gray-200 rounded-lg shadow"
-    href="#"
+  <div
+    class="size-40 md:w-[15rem] h-max max-w-sm bg-white shadow border border-t-gray-500/25 border-x-gray-600/20 rounded-t-xl rounded-b-xl"
   >
-    <img
-      :src="
-        animal.imagen ??
-        (animal.especie === AnimalSpecies.DOG ? dogUrl : catUrl)
-      "
-      alt="animal image"
-      class="h-52 md:size-64 w-full rounded-t-lg"
-    />
-    <div class="pl-4 py-3 pr-3 text-[0.8rem]">
-      <div class="flex gap-2 w-min items-center mb-2">
+    <div>
+      <img
+        :src="
+          animal.imagen ??
+          (animal.especie === AnimalSpecies.DOG ? dogUrl : catUrl)
+        "
+        alt="animal image"
+        class="h-52 md:size-64 w-full rounded-t-xl aspect-square object-cover"
+      />
+    </div>
+
+    <div
+      class="pl-4 pr-4 py-4 text-[0.8rem] shadow-md rounded-b-xl bg-gray-200/45 border border-t-gray-500/15"
+    >
+      <div class="flex justify-between w-full items-center mb-2">
         <h5
-          class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          class="text-[1.42rem] font-bold tracking-tight text-gray-900 dark:text-white"
         >
           {{ animal.nombre }}
         </h5>
-        <img :src="genero" alt="" class="size-6" />
+        <AnimalGenderIcon :gender="animal.genero" />
       </div>
       <div>
         <p class="text-gray-700 dark:text-gray-400">
-          {{ `Aproximadamente ${animal.edad} meses` }}
+          {{ `${animal.edad} meses` }}
         </p>
         <span
           :class="{
             'text-[#11720A] font-semibold': animal.adoptado,
-            'text-red-950 font-semibold': !animal.adoptado,
+            'text-primary font-semibold': !animal.adoptado,
           }"
           >{{ adoptado }}</span
         >
       </div>
     </div>
-  </a>
+  </div>
 </template>
---
+++ src/components/home_page/AnimalGenderIcon.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { AnimalGender } from "@/enums/animal_gender.ts";
+
+const { gender } = defineProps<{ gender: String }>();
+
+const classIcon =
+  gender === AnimalGender.MALE
+    ? "icon-[gg--gender-male] text-[1.7rem]"
+    : "icon-[mdi--gender-female]  text-[1.6rem] text-[#FC49F5]";
+
+const bgGender =
+  gender === AnimalGender.MALE ? "bg-[#8EB1E5]/25" : "bg-[#E58EAD]/25";
+</script>
+
+<template>
+  <div
+    :class="bgGender"
+    class="flex items-center justify-center rounded-full w-[2.3rem] h-[2.3rem]"
+  >
+    <span :class="classIcon" class="text-[#5395F8]" />
+  </div>
+</template>
+
+<style scoped></style>
