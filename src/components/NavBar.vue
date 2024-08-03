3ac0e4a9a54d5fb4a5719f489d1138e0e58dcfd9
Alina Carpio <72272077+alicarpio@users.noreply.github.com>
2024-08-02 22:21:18 -0500

    Add NavBar component (#41)
    * Add SideBar, NavBar components
    
    * Add NavBar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import AmigosConColaLogo from "@assets/images/amigos-con-cola-logo.png";
+</script>
+
+<template>
+  <nav
+    class="w-full sticky bg-white border-[0.005rem] p-2.5 drop-shadow flex justify-between items-center"
+  >
+    <div class="ml-2">
+      <RouterLink class="flex items-center" to="/home">
+        <img
+          :src="AmigosConColaLogo"
+          alt="Logo de Amigos con cola"
+          class="w-10 h-10"
+        />
+        <span class="ml-1 font-medium text-[1.1rem]">Amigos con cola</span>
+      </RouterLink>
+    </div>
+    <div class="flex">
+      <slot />
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
