494bdd87f0d5b619e8fc1bbee0530f3f7f41813c
Alexander Goussas <goussasalexander@gmail.com>
2024-08-04 19:55:20 -0500

    Fix back arrow on pagination controls

--- src/components/common/PaginationControls.vue
+++ src/components/common/PaginationControls.vue
@@ -18,7 +18,7 @@ defineEmits<{
         <span
           class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
           @click="
-            $emit('selectPage', currentPage > 0 ? currentPage - 1 : currentPage)
+            $emit('selectPage', currentPage > 1 ? currentPage - 1 : currentPage)
           "
         >
           <span class="sr-only">Previous</span>
@@ -42,14 +42,14 @@ defineEmits<{
       <li v-for="i in pages">
         <span
           v-if="i === currentPage"
-          class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 bg-primary/20"
+          class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 bg-primary-dark/20"
           @click="$emit('selectPage', i)"
         >
           {{ i }}
         </span>
         <span
           v-else
-          class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700"
+          class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700"
           @click="$emit('selectPage', i)"
         >
           {{ i }}
